e79b9a7c9532b3ce6d1396c2187751fa6ddfde4f
C-Shi <cshi@ualberta.ca>
2018-10-01 16:08:13 -0600

    add charbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+        <h1>Hello React :)</h1>
+        <ChatBar />
+      </div>
+    )
   }
 }
 export default App;
