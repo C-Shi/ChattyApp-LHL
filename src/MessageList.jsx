5d2a82db30cc4dce4dca94b8a73806a9735429a4
C-Shi <cshi@ualberta.ca>
2018-10-01 16:40:25 -0600

    add message to states

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
-class MessageList extends Component {
-  render() {
+const MessageList = (props) => {
+  const messageList = props.messages.map(message => {
     return (
       <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
-    </div>
+        <span className="message-username">{message.username}</span>
+        <span className="message-content">{message.content}</span>
+      </div>
     )
-  }
+  })
+
+  return (
+    <div>
+      {messageList}
+    </div>
+  )
 }
 
 export default MessageList;
