3f1a5bd5e35cd78ef444a11d1d5d3fc61805386b
C-Shi <cshi@ualberta.ca>
2018-10-02 12:37:26 -0600

    component did mount simulate

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,42 @@ import MessageList from './MessageList.jsx';
 import NavBar from './NavBar.jsx';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      currentUser: {
+        name: 'Bob'
+      },
+      message: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id:1
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id:2
+        }
+      ]
+    }
+  }
+
+  componentDidMount(){
+    console.log("Component did mount App");
+    setTimeout( () => {
+      const newMessage = {id: 3, username:'Yimiao', content: 'Meow! Meow!'};
+      const message = this.state.message.concat(newMessage);
+      this.setState({ message })
+    }, 3000)
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.message}/>
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     )
   }
